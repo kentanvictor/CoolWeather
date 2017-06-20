85e8310db4c955b9397cc9b3b0197ed430826016
kentanvictor <2030457926@qq.com>
2017-06-20 17:07:01 +0800

    the layout of the choose area

---
+++ app/src/main/java/com/example/dell/coolweather/Fragment/ChooseAreaFragment.java
@@ -0,0 +1,10 @@
+package com.example.dell.coolweather.Fragment;
+
+import android.app.Fragment;
+
+/**
+ * Created by DELL on 2017/6/20.
+ */
+
+public class ChooseAreaFragment extends Fragment {
+}
