3d73c51ae51a3a116f95aa45d4480cbf8f166af9
kentanvictor <2030457926@qq.com>
2017-06-20 16:36:21 +0800

    Traversing data of provinces, cities and counties of the whole country

--- app/src/main/java/com/example/dell/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/example/dell/coolweather/util/HttpUtil.java
@@ -3,10 +3,10 @@ package com.example.dell.coolweather.util;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 
-/**
- * Created by DELL on 2017/6/20.
+/*
+ * Created by Zoetan on 2017/6/20.
  */
-
+//Interacting with the server
 public class HttpUtil {
     public static void sendOkHttpRequest(String address, okhttp3.Callback callback)
     {
