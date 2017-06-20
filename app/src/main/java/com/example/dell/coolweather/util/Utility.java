4888f2037a564b8c03ad24263a60a2e76527588b
kentanvictor <2030457926@qq.com>
2017-06-20 16:36:48 +0800

    Traversing data of provinces, cities and counties of the whole country

--- app/src/main/java/com/example/dell/coolweather/util/Utility.java
+++ app/src/main/java/com/example/dell/coolweather/util/Utility.java
@@ -16,17 +16,14 @@ import org.json.JSONObject;
 //Parsing and processing the data
 public class Utility {
     /**
-    * parsing and processing the province data from the server
-    */
-    public static boolean handleProvinceResponse(String response)
-    {
-        if(!TextUtils.isEmpty(response))
-        {
+     * parsing and processing the province data from the server
+     */
+    public static boolean handleProvinceResponse(String response) {
+        if (!TextUtils.isEmpty(response)) {
             try {
                 JSONArray allProvicnes = new JSONArray(response);
                 //Use JSONArray to parse the data
-                for(int i = 0;i < allProvicnes.length(); i++)
-                {
+                for (int i = 0; i < allProvicnes.length(); i++) {
                     JSONObject provinceObject = allProvicnes.getJSONObject(i);
                     //Use JSONObject to parse the data
                     province province = new province();
@@ -41,17 +38,15 @@ public class Utility {
         }
         return false;
     }
+
     /**
-    * parsing and processing the city data from the server
-    */
-    public static boolean handleCityResponse(String response,int provinceId)
-    {
-        if(!TextUtils.isEmpty(response))
-        {
+     * parsing and processing the city data from the server
+     */
+    public static boolean handleCityResponse(String response, int provinceId) {
+        if (!TextUtils.isEmpty(response)) {
             try {
                 JSONArray allCities = new JSONArray(response);
-                for(int i = 0;i < allCities.length(); i++)
-                {
+                for (int i = 0; i < allCities.length(); i++) {
                     JSONObject cityObject = allCities.getJSONObject(i);
                     City city = new City();
                     city.setCityName(cityObject.getString("name"));
@@ -65,17 +60,15 @@ public class Utility {
         }
         return false;
     }
+
     /**
      * parsing and processing the county data from the server
      */
-    public static boolean handleCountyResponse(String response,int cityId)
-    {
-        if(!TextUtils.isEmpty(response))
-        {
+    public static boolean handleCountyResponse(String response, int cityId) {
+        if (!TextUtils.isEmpty(response)) {
             try {
                 JSONArray allCounties = new JSONArray(response);
-                for(int i = 0;i < allCounties.length(); i++)
-                {
+                for (int i = 0; i < allCounties.length(); i++) {
                     JSONObject countyObject = allCounties.getJSONObject(i);
                     County county = new County();
                     county.setCountyName(countyObject.getString("name"));
