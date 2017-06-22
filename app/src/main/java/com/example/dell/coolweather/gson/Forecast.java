7b3a7c775d4b8e9be641d5a1f8ea70fe989aa9f7
kentanvictor <2030457926@qq.com>
2017-06-22 11:18:48 +0800

    Show weather information

---
+++ app/src/main/java/com/example/dell/coolweather/gson/Forecast.java
@@ -0,0 +1,23 @@
+package com.example.dell.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Zoetan on 2017/6/22.
+ */
+
+public class Forecast {
+    public String date;
+    @SerializedName("tmp")
+    public Temperature temperature;
+
+    public class Temperature {
+        public String max;
+        public String min;
+    }
+    public  class More
+    {
+        @SerializedName("txt_d")
+        public String info;
+    }
+}
