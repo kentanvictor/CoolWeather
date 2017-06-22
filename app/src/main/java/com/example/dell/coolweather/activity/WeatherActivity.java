ef21bae3c855b4796bfb9357da168093e2ac49ca
kentanvictor <2030457926@qq.com>
2017-06-22 16:34:39 +0800

    Display the weather on the phone screen

--- app/src/main/java/com/example/dell/coolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/example/dell/coolweather/activity/WeatherActivity.java
@@ -12,36 +12,57 @@ import com.example.dell.coolweather.R;
 
 public class WeatherActivity extends AppCompatActivity {
     private ScrollView weatherLayout;
+
     private TextView titleCity;
+
     private TextView titleUpdateTime;
+
     private TextView degressText;
+
     private TextView weatherInfoText;
+
     private LinearLayout forecastLayout;
+
     private TextView aqiText;
+
     private TextView pm25Text;
+
     private TextView comfortText;
+
     private TextView carWashText;
+
     private TextView sportText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
         //Initialize each control
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
+
         titleCity = (TextView) findViewById(R.id.title_city);
+
         titleUpdateTime = (TextView) findViewById(R.id.title_update_time);
+
         degressText = (TextView) findViewById(R.id.degree_text);
+
         weatherInfoText = (TextView) findViewById(R.id.weather_info_text);
+
         forecastLayout = (LinearLayout) findViewById(R.id.forecast_layout);
+
         aqiText = (TextView) findViewById(R.id.aqi_text);
+
         pm25Text = (TextView) findViewById(R.id.pm25_text);
+
         comfortText = (TextView) findViewById(R.id.comfort_text);
+
         carWashText = (TextView) findViewById(R.id.car_wash_text);
+
         sportText = (TextView) findViewById(R.id.sport_text);
+
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
         String weatherString = prefs.getString("weather", null);
-        if(weatherString != null)
-        {
+        if (weatherString != null) {
         }
     }
 }
