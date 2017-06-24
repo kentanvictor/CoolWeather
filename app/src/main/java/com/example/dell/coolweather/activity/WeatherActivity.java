c0eeaacd1504359e7dce53e7f81dca9712ca39b8
kentanvictor <2030457926@qq.com>
2017-06-24 19:46:06 +0800

    Make the background map and status bar merge together

--- app/src/main/java/com/example/dell/coolweather/activity/WeatherActivity.java
+++ app/src/main/java/com/example/dell/coolweather/activity/WeatherActivity.java
@@ -1,6 +1,8 @@
 package com.example.dell.coolweather.activity;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
@@ -52,6 +54,12 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(Build.VERSION.SDK_INT >= 21)
+        {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         //Initialize each control
         weatherLayout = (ScrollView) findViewById(R.id.weather_layout);
